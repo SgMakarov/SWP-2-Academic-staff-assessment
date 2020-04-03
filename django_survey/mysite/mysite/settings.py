cee2acf43902bbf8b3ddd29cbfa82ff377948360
matveyplevako <matveyplevako@rambler.ru>
2020-04-03 22:20:17 +0300

    survey app with authentication

--- django_survey/mysite/mysite/settings.py
+++ django_survey/mysite/mysite/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from os import environ as env
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -24,19 +24,20 @@ SECRET_KEY = '-&=lf#8&wn1wu!ui1d!afb66p(nr%lu=&+ci_uroogeyz1ml)4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'survey.apps.DjangoSurveyAndReportConfig',
+    'students.apps.StudentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "bootstrapform",
 ]
 
 MIDDLEWARE = [
@@ -69,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -83,7 +83,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -103,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -116,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
--- django_survey/mysite/mysite/urls.py
+++ django_survey/mysite/mysite/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from survey.views import IndexView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('survey/', include('survey.urls')),
+    path('students/', include('students.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/profile/', IndexView.as_view()),
     path('admin/', admin.site.urls),
+    path(r'', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
